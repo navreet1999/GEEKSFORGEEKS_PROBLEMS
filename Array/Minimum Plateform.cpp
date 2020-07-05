2753b6c1edd10d8083879b384441236e219b2733
NAVREET KAUR DHIMAN <44254251+navreet1999@users.noreply.github.com>
2020-07-05 10:11:50 +0530

    Add files via upload

---
+++ Array/Minimum Plateform.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include<algorithm> 
+using namespace std;
+
+int findPlatform(int arr[], int dep[], int n) 
+{ 
+// Sort arrival and departure arrays 
+sort(arr, arr+n); 
+sort(dep, dep+n); 
+
+// plat_needed indicates number of platforms 
+// needed at a time 
+int plat_needed = 1, result = 1; 
+int i = 1, j = 0; 
+
+// Similar to merge in merge sort to process 
+// all events in sorted order 
+while (i < n && j < n) 
+{ 
+	// If next event in sorted order is arrival, 
+	// increment count of platforms needed 
+	if (arr[i] <= dep[j]) 
+	{ 
+		plat_needed++; 
+		i++; 
+
+		// Update result if needed 
+		if (plat_needed > result) 
+			result = plat_needed; 
+	} 
+
+	// Else decrement count of platforms needed 
+	else
+	{ 
+		plat_needed--; 
+		j++; 
+	} 
+} 
+
+return result; 
+} 
+
+// Driver program to test methods of graph class 
+int main() 
+{ 
+    int t,size,i;
+    cin>>t;
+    while(t--)
+    {
+        cin>>size;
+        int arr[size];
+        int dep[size];
+        
+        for(i=0;i<size;i++)
+        cin>>arr[i];
+        
+        for(i=0;i<size;i++)
+        cin>>dep[i];
+        
+        cout<<findPlatform(arr,dep,size)<<endl;
+    
+    }
+	
+	return 0; 
+} 
