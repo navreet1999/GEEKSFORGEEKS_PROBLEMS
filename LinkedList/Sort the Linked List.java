322c7b64dc24b88a6e1c531d76c59e466397c069
NAVREET KAUR DHIMAN <44254251+navreet1999@users.noreply.github.com>
2020-07-11 09:42:25 +0530

    Add files via upload

---
+++ LinkedList/Sort the Linked List.java
@@ -0,0 +1,53 @@
+//https://leetcode.com/problems/sort-list/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head==null ||head.next==null)
+            return head;
+        
+        ListNode slow=head;
+        ListNode fast=head;
+        ListNode prev=null;
+        
+        while(fast!=null && fast.next!=null)
+        {
+            prev=slow;
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        prev.next=null;
+        
+        ListNode h1=sortList(head);
+        ListNode h2=sortList(slow);
+          return merge(h1,h2);
+    }
+    public ListNode merge(ListNode h1,ListNode h2)
+    {
+        if(h1==null)
+            return h2;;
+        if(h2==null)
+            return h1;
+        
+        if(h1.val<h2.val)
+        {
+             h1.next=merge(h1.next,h2);
+            return h1;
+        }  
+        else
+        {
+             h2.next=merge(h1,h2.next);
+            return h2;
+        } 
+    }
+    
+}
