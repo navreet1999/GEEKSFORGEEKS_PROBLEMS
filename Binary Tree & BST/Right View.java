addd33ce5e579765d70b9d07c777a8361aad1288
NAVREET KAUR DHIMAN <44254251+navreet1999@users.noreply.github.com>
2020-07-15 17:44:06 +0530

    Add files via upload

---
+++ Binary Tree & BST/Right View.java
@@ -0,0 +1,27 @@
+class Tree
+{
+    static int maxLevel=0;
+    void right(Node root,int level)
+    {
+        if(root==null)
+        return ;
+        
+        if(maxLevel<level)
+         {
+             System.out.print(root.data+" ");
+             maxLevel=level;
+         }
+         
+      
+        right(root.right,level+1);
+        right(root.left,level+1);
+        
+        
+    }
+    void rightView(Node root)
+    {
+      // Your code here
+      maxLevel=0;
+      right(root,1);
+    }
+}
